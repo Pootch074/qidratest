fb06d20f54bde57e1871075dfe1fa877bcab7b84
Richard <dswd.richard@gmail.com>
2025-04-15 10:26:43 +0800

    Additional seeds and db updates

--- resources/views/questionnaires/inc/questions.blade.php
+++ resources/views/questionnaires/inc/questions.blade.php
@@ -24,7 +24,7 @@
                     x-model="checked"
                     class="custom-checkbox"
                 />
-                <span>{{ $mean->means }}</span>
+                <span>{!! $mean->means !!}</span>
             </div>
         @endforeach
     </div>
