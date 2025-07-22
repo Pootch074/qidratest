63cb7c3e5c79fc1dbcb4ba5ac8d64f859cf206a0
Richard <dswd.richard@gmail.com>
2025-07-22 16:11:30 +0800

    Show endorse button

--- resources/views/livewire/rmt-deadlines.blade.php
+++ resources/views/livewire/rmt-deadlines.blade.php
@@ -80,13 +80,13 @@
                             </span>
                         </td>
                         <td class="border border-gray-200 px-4 py-2 text-left">
-                            <a href="#" class="inline-flex items-center gap-1 rounded-full border border-gray-400 px-3 py-1 text-xs text-gray-600 hover:bg-gray-50">
+                            {{-- <a href="#" class="inline-flex items-center gap-1 rounded-full border border-gray-400 px-3 py-1 text-xs text-gray-600 hover:bg-gray-50">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
                                 View
-                            </a>
+                            </a> --}}
 
                             <a href="#" class="inline-flex items-center gap-1 rounded-full border border-gray-400 px-3 py-1 text-xs text-gray-600 hover:bg-gray-50">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
