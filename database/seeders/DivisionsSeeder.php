d239ac41ef46d490ec8e4cda73fbf32eccd82292
Sacrudyme <bladsparrow74@gmail.com>
2025-09-02 00:53:12 +0800

    Updated division and section seeders

--- database/seeders/DivisionsSeeder.php
+++ database/seeders/DivisionsSeeder.php
@@ -19,14 +19,15 @@ class DivisionsSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $divisions = [
+
             [
-                'division_name' => 'Administrative Division',
+                'division_name' => 'Disaster Response Management Division',
                 'office_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'division_name' => 'Disaster Response Management Division',
+                'division_name' => 'GENERAL ADMINISTRATIVE SUPPORT SERVICES DIVISION',
                 'office_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
@@ -50,12 +51,17 @@ class DivisionsSeeder extends Seeder
                 'updated_at' => Carbon::now()
             ],
             [
+                'division_name' => 'Office of the Regional Director',
+                'office_id' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
                 'division_name' => 'Pantawid Pamilyang Pilipino Program Management Division',
                 'office_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
-            ]
-            ,
+            ],
             [
                 'division_name' => 'Policy and Plans Division',
                 'office_id' => 1,
--- database/seeders/SectionsSeeder.php
+++ database/seeders/SectionsSeeder.php
@@ -21,16 +21,112 @@ class SectionsSeeder extends Seeder
 
         $sections = [
             [
-                'section_name' => 'Crisis Intervention Section',
+                'section_name' => 'DISASTER RESPONSE MANAGEMENT SECTION',
                 'division_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-               'section_name' => 'Community-Based Services Section',
-                'division_id' => 1,
+                'section_name' => 'ACCOUNTING SECTION',
+                'division_id' => 3,
                 'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now() 
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'PROPERTY AND SUPPLY SECTION',
+                'division_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'RECORDS AND ARCHIVE MANAGEMENT SECTION',
+                'division_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'HR PERSONNEL ADMINISTRATION SECTION (HRPASS)',
+                'division_id' => 4,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'LEGAL UNIT',
+                'division_id' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'SOCIAL MARKETING UNIT',
+                'division_id' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'SOCIAL TECHNOLOGY UNIT',
+                'division_id' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'NATIONAL HOUSEHOLD TARGETING SECTION',
+                'division_id' => 8,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'POLICY DEVELOPMENT AND PLANNING SECTION',
+                'division_id' => 8,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'STANDARDS SECTION',
+                'division_id' => 8,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'SUSTAINABLE LIVELIHOOD PROGRAM (SLP)',
+                'division_id' => 9,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'CENTER AND RESIDENTIAL CARE FACILITY (CRCF)',
+                'division_id' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'COMMUNITY-BASED SERVICES SECTION (CBSS)',
+                'division_id' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'CRISIS INTERVENTION SECTION',
+                'division_id' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'SOCIAL PENSION PROGRAM',
+                'division_id' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'SUPPLEMENTARY FEEDING PROGRAM',
+                'division_id' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'section_name' => 'PANTAWID PAMILYA PILIPINO PROGRAM MANAGEMENT SECTION',
+                'division_id' => 7,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
         ];
         DB::table('sections')->insert($sections);
