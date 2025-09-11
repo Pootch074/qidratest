72fdb66c56efefe7df4370ba19728fe3db8e4abb
dyme <bladsparrow74@gmail.com>
2025-09-11 14:41:44 +0800

    Apply favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <link rel="icon" type="image/png" href="/dswd.png"> --}}
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     <title>{{ config('app.name', 'DSWD - Qidra') }} - {{ strtoupper(auth()->user()->getUserTypeName()) }} </title>
     @vite(['resources/js/app.js'])
 
