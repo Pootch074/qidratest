d890ae5408dc368586b07d21a1afa84fd7baba50
dyme <bladsparrow74@gmail.com>
2025-09-18 09:10:09 +0800

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/css/custom.scss', 
                 'resources/js/app.js',
                 'resources/js/display.js',
