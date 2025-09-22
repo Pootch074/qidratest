2e6ca86205e009221b4919c3fc3125b7a67002ff
Sacrudyme <bladsparrow74@gmail.com>
2025-09-23 00:23:11 +0800

    Update sidebar logo

--- resources/views/layouts/inc/pacdsidebar.blade.php
+++ resources/views/layouts/inc/pacdsidebar.blade.php
@@ -2,10 +2,14 @@
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div class="h-full px-3 py-4 overflow-y-auto bg-[#2e3192]">
       <div class="flex justify-center mb-6">
+         <img src="{{ Vite::asset('resources/images/dswd-white.png') }}" 
+            alt="App Logo" 
+            class="h-12 w-auto">
          <img src="{{ Vite::asset('resources/images/qidra-logo-white.png') }}" 
-              alt="App Logo" 
-              class="h-16 w-auto">
+            alt="App Logo" 
+            class="h-12 w-auto ml-4">
       </div>
+
 
       <ul class="space-y-2 font-medium">
          <li>
