1b88a58a9f73f9b52763261400d4f90eefb5fe3b
Sacrudyme <bladsparrow74@gmail.com>
2025-10-26 15:47:38 +0800

    API routes are protected.

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UsersController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/register', [\App\Http\Controllers\Api\UsersController::class, 'register']);
+    Route::get('/users', [UsersController::class, 'get'])->name('api-users-get');
+    Route::post('/users', [UsersController::class, 'post'])->name('api-users-post');
+    Route::put('/users/{id}', [UsersController::class, 'put'])->name('api-users-put');
+    Route::delete('/users/{id}', [UsersController::class, 'delete'])->name('api-users-delete');
+});
 
-Route::get('/users', [\App\Http\Controllers\Api\UsersController::class, 'get'])->name('api-users-get');
-Route::post('/users', [\App\Http\Controllers\Api\UsersController::class, 'post'])->name('api-users-post');
-Route::put('/users/{id}', [\App\Http\Controllers\Api\UsersController::class, 'put'])->name('api-users-put');
-Route::delete('/users/{id}', [\App\Http\Controllers\Api\UsersController::class, 'delete'])->name('api-users-delete');
+Route::get('/register', [UsersController::class, 'register']);
