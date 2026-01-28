2f465030f5cf9880c7a4bd84f64567a442856c98
dyme <bladsparrow74@gmail.com>
2026-01-28 09:38:56 +0800

    fix(registration): prevent invalid email DNS from being allowed

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -22,7 +22,7 @@ class RegisterRequest extends FormRequest
             'divisionId' => ['required', 'integer', 'exists:divisions,id'],
             'sectionId' => ['required', 'integer', 'exists:sections,id'],
             'position' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users,email', 'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/'],
+            'email' => ['required', 'email:rfc,dns', 'max:255', 'unique:users,email', 'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/'],
             'stepId' => ['required', 'integer', 'exists:steps,id'],
             'windowId' => ['required', 'integer', 'exists:windows,id'],
             'category' => ['required', 'string', 'max:255'],
